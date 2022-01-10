5ae9baba2b94d645981f4d3a8b753dfb8fc4ceca
Andre Alexander Bell <github@andre-bell.de>
2022-01-10 05:46:31 +0100

    Initial Commit

---
+++ Gallaugher_You-Are-Awesome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gallaugher_You-Are-Awesome
+//
+//  Created by Andre Bell on 10.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
